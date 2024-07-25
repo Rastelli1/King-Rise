0fa9fc96fbff7b773a7cdd20062ca4db7606e1e3
Rastelli <rastelliruth@gmail.com>
2024-07-25 16:07:22 -0300

    Modo Pausa

---
+++ King Rise/Assets/Scrips/GameManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //[SerializeField] GameObject panel;
+
+    private bool isPaused=false;
+
+    public static GameManager instance {  get; private set; }
+
+    private void Awake()
+    {
+        if(instance != null && instance!= this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PausedState();
+        }
+        
+    }
+    private void PausedState()
+    {
+        isPaused = !isPaused;
+
+        if (isPaused)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+    }
+
+    //private void  PanelPaused()
+    //{
+    //    if (isPaused)
+    //    {
+    //        panel.SetActive(true);
+    //    }
+    //    else
+    //    {
+    //        panel.SetActive(false);
+    //    }
+    //}
+}
