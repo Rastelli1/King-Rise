747250081601c37aab915f42bfd1852ed34ec855
Rastelli <rastelliruth@gmail.com>
2024-08-06 17:09:16 -0300

    Cambios de particulas

--- King Rise/Assets/Scrips/obstacle/Spawn Obstacles.cs
+++ King Rise/Assets/Scrips/obstacle/Spawn Obstacles.cs
@@ -33,7 +33,7 @@ public class SpawnObstacles : MonoBehaviour
 
     public void PositionGenerate() //genera una posicion aleatoria en el eje x
     {
-        float spawnPositionX= Random.Range(-3.2f , 3.5f);
+        float spawnPositionX= Random.Range(-3.6f , 3.5f);
         Vector3 randompos= new Vector3 (spawnPositionX, pointEjeY.position.y);
         Instantiate(ObstaclesRandom(), randompos, Quaternion.identity);
     }
